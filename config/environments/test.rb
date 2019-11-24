b2363a53691c2f2ac85d9d22b1de4d032eabd657
github username <github email address>
2019-11-24 15:10:36 -0800

    Done.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,6 +36,7 @@ Rails.application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+  config.action_view.automatically_disable_submit_tag = false
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
